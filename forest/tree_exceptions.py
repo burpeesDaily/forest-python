b082a83aded45b02ce5a63acc0c39b86a28bfa7f
Shun's Vineyard <shunsvineyard@protonmail.com>
2021-02-24 23:41:37 -0800

    Simplified tree_exceptions.py.

--- forest/tree_exceptions.py
+++ forest/tree_exceptions.py
@@ -1,15 +1,8 @@
-# Copyright © 2020 by Shun Huang. All rights reserved.
+# Copyright © 2021 by Shun Huang. All rights reserved.
 # Licensed under MIT License.
 # See LICENSE in the project root for license information.
 
 """Tree Exception Definitions."""
-
-
-class EmptyTreeError(Exception):
-    """Raised when a tree is empty."""
-
-    def __init__(self) -> None:
-        Exception.__init__(self, "The tree is empty.")
 
 
 class DuplicateKeyError(Exception):
@@ -17,10 +10,3 @@ class DuplicateKeyError(Exception):
 
     def __init__(self, key: str) -> None:
         Exception.__init__(self, f"{key} already exists.")
-
-
-class KeyNotFoundError(Exception):
-    """Raised when a key does not exist."""
-
-    def __init__(self, key: str) -> None:
-        Exception.__init__(self, f"{key} does not exist.")

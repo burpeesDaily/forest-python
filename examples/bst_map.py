9bb868a85185123376c93ae85b93d93182d89b4d
Shun's Vineyard <shunsvineyard@protonmail.com>
2021-03-08 23:12:24 -0800

    Added some comments.

--- examples/bst_map.py
+++ examples/bst_map.py
@@ -16,15 +16,18 @@ class Map:
         self._bst = binary_search_tree.BinarySearchTree()
 
     def __setitem__(self, key: Any, value: Any) -> None:
+        """Insert (key, value) item into the map."""
         self._bst.insert(key=key, data=value)
 
     def __getitem__(self, key: Any) -> Optional[Any]:
+        """Get the data by the given key."""
         node = self._bst.search(key=key)
         if node:
             return node.data
         return None
 
     def __delitem__(self, key: Any) -> None:
+        """Remove a (key, value) pair from the map."""
         self._bst.delete(key=key)
 
     @property

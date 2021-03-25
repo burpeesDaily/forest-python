b9f938a10f1d95f07d375e4731f44695c27ee538
Shun's Vineyard <shunsvineyard@protonmail.com>
2021-03-25 00:01:51 -0700

    Implemented single right threaded binary search tree.

---
+++ tests/test_single_thread_binary_tree.py
@@ -0,0 +1,77 @@
+"""Unit tests for the single thread binary search tree module."""
+
+from forest.binary_trees import single_thread_binary_tree
+
+
+def test_simple_right_threaded_case(basic_tree):
+    """Test the basic opeartions of a right threaded binary search tree."""
+    tree = single_thread_binary_tree.RightThreadedBinaryTree()
+
+    # 23, 4, 30, 11, 7, 34, 20, 24, 22, 15, 1
+    for key, data in basic_tree:
+        tree.insert(key=key, data=data)
+
+    assert [(1, "1"), (4, "4"), (7, "7"), (11, "11"), (15, "15"), (20, "20"),
+            (22, "22"), (23, "23"), (24, "24"), (30, "30"), (34, "34")] == \
+           [item for item in tree.inorder_traverse()]
+
+    assert [(23, "23"), (4, "4"), (1, "1"), (11, "11"), (7, "7"), (20, "20"),
+            (15, "15"), (22, "22"), (30, "30"), (24, "24"), (34, "34")] == \
+           [item for item in tree.preorder_traverse()]
+
+    assert tree.get_leftmost(node=tree.root).key == 1
+    assert tree.get_rightmost(node=tree.root).key == 34
+    assert tree.search(key=24).data == "24"
+
+    tree.delete(key=34)
+    assert tree.search(key=34) is None
+
+    tree.delete(key=15)
+    tree.delete(key=22)
+    tree.delete(key=7)
+    tree.delete(key=20)
+
+    assert tree.search(key=15) is None
+
+    assert [(1, "1"), (4, "4"), (11, "11"), (23, "23"), (24, "24"),
+            (30, "30")] == [item for item in tree.inorder_traverse()]
+
+
+def test_deletion_right_threaded_case(basic_tree):
+    """Test the deletion of a right threaded binary search tree."""
+    tree = single_thread_binary_tree.RightThreadedBinaryTree()
+
+    # 23, 4, 30, 11, 7, 34, 20, 24, 22, 15, 1
+    for key, data in basic_tree:
+        tree.insert(key=key, data=data)
+
+    # No child
+    tree.delete(15)
+    assert [(1, "1"), (4, "4"), (7, "7"), (11, "11"), (20, "20"), (22, "22"),
+            (23, "23"), (24, "24"), (30, "30"), (34, "34")] == \
+           [item for item in tree.inorder_traverse()]
+
+    # One right child
+    tree.delete(20)
+    assert [(1, "1"), (4, "4"), (7, "7"), (11, "11"), (22, "22"), (23, "23"),
+            (24, "24"), (30, "30"), (34, "34")] == \
+           [item for item in tree.inorder_traverse()]
+
+    # One left child
+    tree.insert(key=17, data="17")
+    tree.delete(22)
+    assert [(1, "1"), (4, "4"), (7, "7"), (11, "11"), (17, "17"), (23, "23"),
+            (24, "24"), (30, "30"), (34, "34")] == \
+           [item for item in tree.inorder_traverse()]
+
+    # Two children
+    tree.delete(11)
+    assert [(1, "1"), (4, "4"), (7, "7"), (17, "17"), (23, "23"), (24, "24"),
+            (30, "30"), (34, "34")] == \
+           [item for item in tree.inorder_traverse()]
+
+    # Delete the root
+    tree.delete(23)
+    assert [(1, "1"), (4, "4"), (7, "7"), (17, "17"), (24, "24"),
+            (30, "30"), (34, "34")] == \
+           [item for item in tree.inorder_traverse()]

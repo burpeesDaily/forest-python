b15bed61111b83d45dceda998643b139bb644e54
Shun's Vineyard <shunsvineyard@protonmail.com>
2021-06-04 20:27:25 -0700

    Added AVL tree example.

---
+++ examples/avlt_map.py
@@ -0,0 +1,65 @@
+# Copyright © 2021 by Shun Huang. All rights reserved.
+# Licensed under MIT License.
+# See LICENSE in the project root for license information.
+
+"""The module demonstrates using an AVL Tree to implement Map."""
+
+from typing import Any, Optional
+
+from forest.binary_trees import avl_tree
+from forest.binary_trees import traversal
+
+
+class Map:
+    """Key-value Map implemented using AVL Tree."""
+
+    def __init__(self) -> None:
+        self._avlt = avl_tree.AVLTree()
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        """Insert (key, value) item into the map."""
+        self._avlt.insert(key=key, data=value)
+
+    def __getitem__(self, key: Any) -> Optional[Any]:
+        """Get the data by the given key."""
+        node = self._avlt.search(key=key)
+        if node:
+            return node.data
+        return None
+
+    def __delitem__(self, key: Any) -> None:
+        """Remove a (key, value) pair from the map."""
+        self._avlt.delete(key=key)
+
+    def __iter__(self) -> traversal.Pairs:
+        """Iterate the data in the map."""
+        return traversal.inorder_traverse(tree=self._avlt)
+
+    @property
+    def empty(self) -> bool:
+        """Return `True` if the map is empty; `False` otherwise."""
+        return self._avlt.empty
+
+
+if __name__ == "__main__":
+
+    # Initialize the Map instance.
+    contacts = Map()
+
+    # Add some items.
+    contacts["Mark"] = "mark@email.com"
+    contacts["John"] = "john@email.com"
+    contacts["Luke"] = "luke@email.com"
+
+    # Retrieve an email
+    print(contacts["Mark"])
+
+    # Delete one item.
+    del contacts["John"]
+
+    # Check the deleted item.
+    print(contacts["John"])  # This will print None
+
+    # Iterate the items.
+    for contact in contacts:
+        print(contact)

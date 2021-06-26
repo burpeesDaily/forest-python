7286256b6415d4c9bb89bd025e9ff73d92211453
Shun's Vineyard <shunsvineyard@protonmail.com>
2021-06-25 21:41:47 -0700

    Added more tests.

--- tests/test_red_black_tree.py
+++ tests/test_red_black_tree.py
@@ -236,18 +236,19 @@ def test_red_black_tree_traversal(basic_tree):
 
 def test_random_insert_delete():
     """Test random insert and delete."""
-    insert_data = random.sample(range(1, 2000), 1000)
-    delete_data = random.sample(insert_data, 500)
+    for _ in range(0, 10):
+        insert_data = random.sample(range(1, 2000), 1000)
+        delete_data = random.sample(insert_data, 500)
 
-    remaining_data = [item for item in insert_data if item not in delete_data]
-    remaining_data.sort()
+        remaining_data = [item for item in insert_data if item not in delete_data]
+        remaining_data.sort()
 
-    tree = red_black_tree.RBTree()
-    for key in insert_data:
-        tree.insert(key=key, data=str(key))
+        tree = red_black_tree.RBTree()
+        for key in insert_data:
+            tree.insert(key=key, data=str(key))
 
-    for key in delete_data:
-        tree.delete(key=key)
+        for key in delete_data:
+            tree.delete(key=key)
 
-    result = [item for item, _ in tree.inorder_traverse()]
-    assert result == remaining_data
+        result = [item for item, _ in tree.inorder_traverse()]
+        assert result == remaining_data
